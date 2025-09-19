ebfb054673ec892ba97c993afc8c0e6e1ce828db
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-19 14:29:42 +0900

    Enable to start empty server

--- src/azarole.rs
+++ src/azarole.rs
@@ -1,2 +1,10 @@
-fn main() {
+use actix_web::{App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> anyhow::Result<()> {
+    let server = HttpServer::new(move || {
+        App::new()
+    });
+    server.bind(("127.0.0.1", 3000))?.run().await?;
+    Ok(())
 }
