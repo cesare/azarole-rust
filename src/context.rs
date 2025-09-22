23b0658b3d04de86f4f0c37839642986e1299d7f
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-22 15:34:33 +0900

    Add database context

--- src/context.rs
+++ src/context.rs
@@ -1,16 +1,32 @@
 use anyhow::Result;
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
 
 use crate::config::ApplicationConfig;
 
 #[derive(Clone)]
+pub struct DatabaseContext {
+    pub pool: Pool<Sqlite>,
+}
+
+impl DatabaseContext {
+    fn new(config: &ApplicationConfig) -> Result<Self> {
+        let pool = SqlitePoolOptions::new().connect_lazy(&config.database.url)?;
+        Ok(Self { pool })
+    }
+}
+
+#[derive(Clone)]
 pub struct ApplicationContext {
     pub config: ApplicationConfig,
+    pub database: DatabaseContext,
 }
 
 impl ApplicationContext {
     pub fn new(config: &ApplicationConfig) -> Result<Self> {
+        let database = DatabaseContext::new(config)?;
         let context = Self {
             config: config.clone(),
+            database
         };
         Ok(context)
     }
