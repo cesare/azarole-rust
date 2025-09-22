f10b125934f2dab0e3d19a93929ebaec6ec76e63
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-22 10:02:00 +0900

    Read configuration file to start server process

--- src/azarole.rs
+++ src/azarole.rs
@@ -2,6 +2,9 @@ use std::path::PathBuf;
 
 use actix_web::{App, HttpServer};
 use clap::Parser;
+use serde::Deserialize;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -9,13 +12,29 @@ struct Args {
     config_file: PathBuf,
 }
 
+#[derive(Deserialize)]
+struct ServerConfig {
+    bind: String,
+    port: u16,
+}
+
+#[derive(Deserialize)]
+struct ApplicationConfig {
+    server: ServerConfig,
+}
+
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
-    let _args = Args::parse();
+    let args = Args::parse();
+
+    let mut file = File::open(&args.config_file).await?;
+    let mut content = String::new();
+    file.read_to_string(&mut content).await?;
+    let config: ApplicationConfig = toml::from_str(&content)?;
 
     let server = HttpServer::new(move || {
         App::new()
     });
-    server.bind(("127.0.0.1", 3000))?.run().await?;
+    server.bind((config.server.bind, config.server.port))?.run().await?;
     Ok(())
 }
