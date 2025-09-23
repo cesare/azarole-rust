f42d8b6deb8b70a19d23437d3791e58d7c6129fc
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-23 14:41:45 +0900

    Enable logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use actix_web::middleware::Logger;
 use actix_web::web::{scope, Data};
 use actix_web::{App, HttpServer};
+use env_logger::Env;
 
 mod args;
 mod config;
@@ -11,12 +13,15 @@ use self::context::ApplicationContext;
 
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let args = args::parse();
     let config = ApplicationConfig::load(&args).await?;
     let context = ApplicationContext::new(&config)?;
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
             .app_data(Data::new(context.clone()))
             .service(
                 scope("/api").configure(handlers::api::routes)
