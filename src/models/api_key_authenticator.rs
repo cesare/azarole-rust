558bce6fe284b6b64c7257f83a5e2315904dbb37
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-24 16:32:28 +0900

    Remove context field

--- src/models/api_key_authenticator.rs
+++ src/models/api_key_authenticator.rs
@@ -4,19 +4,17 @@ use crate::context::ApplicationContext;
 use crate::models::user::User;
 
 pub struct ApiKeyAuthenticator {
-    context: ApplicationContext,
     token: String,
 }
 
 impl ApiKeyAuthenticator {
-    pub fn new(context: ApplicationContext, token: &str) -> Self {
+    pub fn new(token: &str) -> Self {
         Self {
-            context,
             token: token.to_owned(),
         }
     }
 
-    pub async fn authenticate(&self) -> Result<User> {
+    pub async fn authenticate(&self, context: &ApplicationContext) -> Result<User> {
         Ok(User::new(1))
     }
 }
