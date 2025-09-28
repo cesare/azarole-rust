b14af7cfea908dcc90d00868566f01d6369922e4
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-28 16:22:33 +0900

    Enable to create clock-in record

--- src/handlers/api.rs
+++ src/handlers/api.rs
@@ -2,10 +2,12 @@ use std::ops::Deref;
 
 use actix_web::web::{post, Data, Path, ReqData, ServiceConfig};
 use actix_web::{HttpResponse, Result};
+use chrono::Utc;
 use serde_json::json;
 
 use crate::context::ApplicationContext;
 use crate::errors::{DatabaseError, PerRequestError};
+use crate::models::attendance_record::AttendanceRecord;
 use crate::models::user::User;
 
 pub fn routes(config: &mut ServiceConfig) {
@@ -17,9 +19,9 @@ async fn clock_in(context: Data<ApplicationContext>, current_user: ReqData<User>
     let workplace_id = path.into_inner();
     let _ = ensure_workplace(context.get_ref(), current_user.deref(), workplace_id).await?;
 
+    let attendance_record = create_clock_in(context.get_ref(), workplace_id).await?;
     let response_json = json!({
-        "user_id": current_user.id,
-        "workplace_id": workplace_id,
+        "attendanceRecord": attendance_record,
     });
     let response = HttpResponse::Created().json(response_json);
     Ok(response)
@@ -33,6 +35,22 @@ async fn ensure_workplace(context: &ApplicationContext, user: &User, workplace_i
         .await;
     match result {
         Ok(_) => Ok(()),
+        Err(error) => Err(error.into()),
+    }
+}
+
+async fn create_clock_in(context: &ApplicationContext, workplace_id: u32) -> Result<AttendanceRecord, DatabaseError> {
+    let now = Utc::now();
+    let statement = "insert into attendance_records (workplace_id, event, recorded_at, created_at) values($1, $2, $3, $4) returning id, workplace_id, event, recorded_at";
+    let result = sqlx::query_as::<sqlx::Sqlite, AttendanceRecord>(statement)
+        .bind(workplace_id)
+        .bind("clock-in")
+        .bind(now)
+        .bind(now)
+        .fetch_one(&context.database.pool)
+        .await;
+    match result {
+        Ok(attendance_record) => Ok(attendance_record),
         Err(error) => Err(error.into()),
     }
 }
