19fee912eddea245fdbd2646443a2bfc56291a3d
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-28 19:36:19 +0900

    Enable to clock-out

--- src/handlers/api.rs
+++ src/handlers/api.rs
@@ -12,14 +12,23 @@ use crate::models::user::User;
 
 pub fn routes(config: &mut ServiceConfig) {
     config
-        .route("/workplaces/{workplace_id}/clock_ins", post().to(clock_in));
+        .route("/workplaces/{workplace_id}/clock_ins", post().to(clock_in))
+        .route("/workplaces/{workplace_id}/clock_outs", post().to(clock_out));
 }
 
 async fn clock_in(context: Data<ApplicationContext>, current_user: ReqData<User>, path: Path<u32>) -> Result<HttpResponse, PerRequestError> {
+    create_clock(context, current_user, path, Event::ClockIn).await
+}
+
+async fn clock_out(context: Data<ApplicationContext>, current_user: ReqData<User>, path: Path<u32>) -> Result<HttpResponse, PerRequestError> {
+    create_clock(context, current_user, path, Event::ClockOut).await
+}
+
+async fn create_clock(context: Data<ApplicationContext>, current_user: ReqData<User>, path: Path<u32>, event: Event) -> Result<HttpResponse, PerRequestError> {
     let workplace_id = path.into_inner();
     ensure_workplace(context.get_ref(), current_user.deref(), workplace_id).await?;
 
-    let attendance_record = create_clock_in(context.get_ref(), workplace_id).await?;
+    let attendance_record = create_attendance(context.get_ref(), workplace_id, event).await?;
     let response_json = json!({
         "attendanceRecord": {
             "id": attendance_record.id,
@@ -44,12 +53,12 @@ async fn ensure_workplace(context: &ApplicationContext, user: &User, workplace_i
     }
 }
 
-async fn create_clock_in(context: &ApplicationContext, workplace_id: u32) -> Result<AttendanceRecord, DatabaseError> {
+async fn create_attendance(context: &ApplicationContext, workplace_id: u32, event: Event) -> Result<AttendanceRecord, DatabaseError> {
     let now = Utc::now();
     let statement = "insert into attendance_records (workplace_id, event, recorded_at, created_at) values($1, $2, $3, $4) returning id, workplace_id, event, recorded_at";
     let result = sqlx::query_as::<sqlx::Sqlite, AttendanceRecord>(statement)
         .bind(workplace_id)
-        .bind(Event::ClockIn)
+        .bind(event)
         .bind(now)
         .bind(now)
         .fetch_one(&context.database.pool)
