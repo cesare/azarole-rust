41161993ff0bd670a93e3063eef45b8f60ff9c73
SAWADA Tadashi <cesare@mayverse.jp>
2025-10-01 10:12:00 +0900

    Set CORS headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_cors::Cors;
+use actix_web::http::header;
 use actix_web::middleware::Logger;
 use actix_web::web::{scope, Data};
 use actix_web::{App, HttpServer};
@@ -15,6 +17,14 @@ use self::config::ApplicationConfig;
 use self::context::ApplicationContext;
 use self::middlewares::require_api_key::RequireApiKey;
 
+fn build_cors(config: &ApplicationConfig) -> Cors {
+    Cors::default()
+        .allowed_origin(&config.frontend.base_url)
+        .allowed_methods(vec!["POST", "GET", "DELETE", "OPTIONS"])
+        .allowed_headers(vec![header::CONTENT_TYPE])
+        .supports_credentials()
+}
+
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -22,10 +32,12 @@ async fn main() -> anyhow::Result<()> {
     let args = args::parse();
     let config = ApplicationConfig::load(&args).await?;
     let context = ApplicationContext::new(&config)?;
+    let server_config = config.server.clone();
 
     let server = HttpServer::new(move || {
         App::new()
             .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
+            .wrap(build_cors(&config))
             .app_data(Data::new(context.clone()))
             .service(
                 scope("/api")
@@ -33,6 +45,6 @@ async fn main() -> anyhow::Result<()> {
                     .configure(handlers::api::routes)
             )
     });
-    server.bind((config.server.bind, config.server.port))?.run().await?;
+    server.bind((server_config.bind, server_config.port))?.run().await?;
     Ok(())
 }
