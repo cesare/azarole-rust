440cd15487ae6d77442cee1bcfbec9d326b30a59
SAWADA Tadashi <cesare@mayverse.jp>
2025-10-03 11:47:31 +0900

    [wip] handle callback

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,31 +1,35 @@
+use std::sync::Arc;
+
 use actix_session::Session;
 use actix_web::{
-    HttpResponse,
-    http::header,
-    web::{Data, ServiceConfig, post},
+    http::header, web::{get, post, Data, Query, ServiceConfig}, HttpResponse
 };
+use log::debug;
+use serde::Deserialize;
+use thiserror::Error;
 
 use crate::{
     context::ApplicationContext,
     errors::PerRequestError,
 };
 
+mod access_token_request;
 mod authentication_request;
+use access_token_request::AccessTokenRequest;
 use authentication_request::AuthenticationRequestGenerator;
 
 pub fn routes(config: &mut ServiceConfig) {
     config
         .route("", post().to(request_authentication))
-        .route("/callback", post().to(callback));
+        .route("/callback", get().to(callback));
 }
 
 async fn request_authentication(context: Data<ApplicationContext>, session: Session) -> Result<HttpResponse, PerRequestError> {
     let generator = AuthenticationRequestGenerator::new(&context.config);
     let authentication_request = generator.generate();
 
-    if let Err(_) = session.insert("google-auth-state", &authentication_request.state) {
-        return Err(PerRequestError::ServerError)
-    }
+    session.insert("google-auth-state", &authentication_request.state)?;
+    session.insert("google-auth-nonce", &authentication_request.nonce)?;
 
     let response = HttpResponse::Found()
         .insert_header((header::LOCATION, authentication_request.request_url))
@@ -33,7 +37,77 @@ async fn request_authentication(context: Data<ApplicationContext>, session: Sess
     Ok(response)
 }
 
+#[derive(Deserialize)]
+struct CallbackParameters {
+    code: Option<String>,
+    state: Option<String>,
+    error: Option<String>,
+}
+
+async fn callback(context: Data<ApplicationContext>, session: Session, params: Query<CallbackParameters>) -> Result<HttpResponse, PerRequestError> {
+    let parameters = params.into_inner();
+    match parameters {
+        CallbackParameters { code: Some(code), state: Some(state), error: None } => {
+            handle_success(context, session, code, state).await
+        },
+        _ => {
+            handle_failure(context, session, parameters.error).await
+        },
+    }
+}
+
 #[allow(unused_variables)]
-async fn callback(context: Data<ApplicationContext>, session: Session) -> Result<HttpResponse, PerRequestError> {
-    todo!()
+async fn handle_success(context: Data<ApplicationContext>, session: Session, code: String, state: String) -> Result<HttpResponse, PerRequestError> {
+    debug!("callback: success {}, {}", code, state);
+
+    let saved_nonce = fetch_saved_string(&session, "google-auth-nonce")?;
+    let saved_state = fetch_saved_string(&session, "google-auth-state")?;
+    if state != saved_state {
+        return Err(PerRequestError::Unauthorized)
+    }
+
+    let access_token_request = AccessTokenRequest::new(Arc::clone(&context.into_inner()));
+    let access_token_response = access_token_request.execute(&code).await?;
+
+    session.clear();
+    session.renew();
+
+    let response = HttpResponse::Ok().finish();
+    Ok(response)
+}
+
+#[allow(unused_variables)]
+async fn handle_failure(context: Data<ApplicationContext>, session: Session, error: Option<String>) -> Result<HttpResponse, PerRequestError> {
+    debug!("callback: failure {}", error.unwrap_or_default());
+
+    session.remove("google-auth-state");
+    session.remove("google-auth-nonce");
+
+    let response = HttpResponse::Unauthorized().finish();
+    Ok(response)
+}
+
+fn fetch_saved_string(session: &Session, key: &str) -> Result<String, PerRequestError> {
+    match session.get::<String>(key)? {
+        Some(state) => Ok(state),
+        _ => Err(PerRequestError::Unauthorized),
+    }
+}
+
+#[derive(Debug, Error)]
+enum AuthError {
+    #[error("request failed")]
+    RequestFailed,
+}
+
+impl From<reqwest::Error> for AuthError {
+    fn from(_value: reqwest::Error) -> Self {
+        Self::RequestFailed
+    }
+}
+
+impl From<AuthError> for PerRequestError {
+    fn from(_value: AuthError) -> Self {
+        PerRequestError::ServerError
+    }
 }
---
+++ src/handlers/auth/access_token_request.rs
@@ -0,0 +1,61 @@
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    context::ApplicationContext,
+    secrets::Secrets,
+};
+use super::AuthError;
+
+#[derive(Serialize)]
+struct Parameters {
+    client_id: String,
+    client_secret: String,
+    code: String,
+    grant_type: String,
+    redirect_uri: String,
+}
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+pub(super) struct AccessTokenResponse {
+    access_token: String,
+    id_token: String,
+    expires_in: u32,
+    scope: String,
+    token_type: String,
+}
+
+pub(super) struct AccessTokenRequest {
+    context: Arc<ApplicationContext>,
+}
+
+impl AccessTokenRequest {
+    pub(super) fn new(context: Arc<ApplicationContext>) -> Self {
+        Self { context }
+    }
+
+    pub(super) async fn execute(&self, code: &str) -> Result<AccessTokenResponse, AuthError> {
+        let secrets = Secrets::default();
+        let client_id = secrets.google_auth.client_id().clone();
+        let client_secret = secrets.google_auth.client_secret().clone();
+        let redirect_uri = format!("{}/auth/google/callback", self.context.config.app.base_url);
+
+        let parameters = Parameters {
+            client_id,
+            client_secret,
+            code: code.to_owned(),
+            grant_type: "authorization_code".to_owned(),
+            redirect_uri,
+        };
+
+        let client = reqwest::Client::new();
+        let raw_response = client.post("https://oauth2.googleapis.com/token")
+            .form(&parameters)
+            .send()
+            .await?;
+        let response = raw_response.json::<AccessTokenResponse>().await?;
+        Ok(response)
+    }
+}
