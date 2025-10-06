7a275129c1fa82589c97de0ef4ce3af348c30f09
SAWADA Tadashi <cesare@mayverse.jp>
2025-10-06 12:39:03 +0900

    Add Workplace and WorkplaceResources

--- src/models.rs
+++ src/models.rs
@@ -7,5 +7,5 @@ pub mod workplace;
 pub use api_key::ApiKey;
 pub use attendance_record::AttendanceRecord;
 pub use attendance_registration::AttendanceRegistration;
-pub use user::User;
-pub use workplace::WorkplaceId;
+pub use user::{User, UserId};
+pub use workplace::{Workplace, WorkplaceId};
--- src/models/workplace.rs
+++ src/models/workplace.rs
@@ -1,6 +1,37 @@
 use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+
+use crate::{context::ApplicationContext, errors::DatabaseError};
+
+use super::{User, UserId};
 
 #[derive(Clone, Copy, Deserialize, Serialize, sqlx::Type)]
 #[sqlx(transparent)]
 #[repr(transparent)]
 pub struct WorkplaceId(u32);
+
+#[derive(Clone, Deserialize, FromRow, Serialize)]
+pub struct Workplace {
+    id: WorkplaceId,
+    user_id: UserId,
+    name: String,
+}
+
+pub struct WorkplaceResources<'a> {
+    context: &'a ApplicationContext,
+    user: &'a User,
+}
+
+impl<'a> WorkplaceResources<'a> {
+    pub fn new(context: &'a ApplicationContext, user: &'a User) -> Self {
+        Self { context, user }
+    }
+
+    pub async fn list(&self) -> Result<Vec<Workplace>, DatabaseError> {
+        let workplaces: Vec<Workplace> = sqlx::query_as("select id, user_id, name from workplaces where user_id = $1")
+            .bind(self.user.id)
+            .fetch_all(&self.context.database.pool)
+            .await?;
+        Ok(workplaces)
+    }
+}
