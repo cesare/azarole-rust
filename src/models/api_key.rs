7c5054646f3e3d2f22f2ac39b9ad889536a50fe7
SAWADA Tadashi <cesare@mayverse.jp>
2025-10-07 10:48:51 +0900

    Refactor ApiKey

--- src/models/api_key.rs
+++ src/models/api_key.rs
@@ -1,11 +1,17 @@
+use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
 use crate::models::user::UserId;
 
-#[derive(FromRow)]
+#[derive(Clone, Copy, Deserialize, Serialize, sqlx::Type)]
+#[sqlx(transparent)]
+#[repr(transparent)]
+pub struct ApiKeyId(u32);
+
+#[derive(Clone, Deserialize, FromRow, Serialize)]
 #[allow(dead_code)]
 pub struct ApiKey {
-    pub id: u32,
+    pub id: ApiKeyId,
     pub user_id: UserId,
     pub name: String,
     pub digest: String,
