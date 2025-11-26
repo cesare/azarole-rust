1e944c9d2bcbbfbacf9e5ab2cae03a597228ae43
SAWADA Tadashi <cesare@mayverse.jp>
2025-11-26 12:30:57 +0900

    Add base64 encoded type

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,6 +1,31 @@
 use std::env;
 use anyhow::Result;
 use base64::{engine::general_purpose::STANDARD, Engine as _};
+use serde::{de, Deserialize};
+
+#[derive(Clone)]
+struct Base64Encoded(Vec<u8>);
+
+impl<'de> Deserialize<'de> for Base64Encoded {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de> {
+        deserializer.deserialize_string(Base64EncodedVisitor)
+    }
+}
+
+struct Base64EncodedVisitor;
+impl<'de> de::Visitor<'de> for Base64EncodedVisitor {
+    type Value = Base64Encoded;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a base64 encoded string")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: de::Error {
+        STANDARD.decode(v).map(Base64Encoded).map_err(E::custom)
+    }
+}
 
 #[derive(Clone, Default)]
 pub struct ApikeyConfig;
