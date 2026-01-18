82af05a5f0d20c9cb6442730547b068eb94be78a
SAWADA Tadashi <cesare@mayverse.jp>
2026-01-18 10:03:03 +0900

    Add tests for /workplaces

---
+++ tests/fixtures/workplaces.sql
@@ -0,0 +1,6 @@
+insert into workplaces (id, user_id, name, created_at, updated_at)
+values
+  (1, 1, 'workplace-01-for-user-01', '2026-01-09T12:34:56Z', '2026-01-09T12:34:56Z'),
+  (2, 1, 'workplace-02-for-user-01', '2026-01-09T12:34:56Z', '2026-01-09T12:34:56Z'),
+  (3, 2, 'workplace-03-for-user-02', '2026-01-10T11:22:33Z', '2026-01-10T11:22:33Z')
+;
