5b9f49bcf1aff9ce0578e675f64c1fc5dbe7bacf
SAWADA Tadashi <cesare@mayverse.jp>
2026-01-19 09:41:11 +0900

    Add tests for POST /workplaces

--- tests/workplaces_test.rs
+++ tests/workplaces_test.rs
@@ -1,4 +1,6 @@
 use actix_web::{App, http::StatusCode, test, web::Data};
+use azarole::{models::User, repositories::RepositoryFactory};
+use serde::Serialize;
 use serde_json::{Value, json};
 use sqlx::SqlitePool;
 
@@ -59,4 +61,81 @@ async fn workplace_listing(pool: SqlitePool) {
         ],
     });
     assert_eq!(response_json, expected_json);
+}
+
+#[sqlx::test(fixtures("users"))]
+async fn workplace_creation_without_signin(pool: SqlitePool) {
+    let context = common::create_context(pool);
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    #[derive(Serialize)]
+    struct Params {
+        name: String,
+    }
+
+    let request = test::TestRequest::post()
+        .uri("/workplaces")
+        .set_form(Params {
+            name: "test-workplace".to_owned(),
+        })
+        .to_request();
+    let result = test::try_call_service(&app, request).await;
+
+    assert!(result.is_err());
+
+    let error = result.unwrap_err();
+    let response = error.error_response();
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+}
+
+#[sqlx::test(fixtures("users"))]
+async fn workplace_creation(pool: SqlitePool) {
+    let context = common::create_context(pool);
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context.clone()))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    #[derive(Serialize)]
+    struct Params {
+        name: String,
+    }
+
+    let cookie_value = common::generate_cookie_value_with_signin_user(1);
+    let request = test::TestRequest::post()
+        .uri("/workplaces")
+        .insert_header(("Cookie", cookie_value))
+        .set_form(Params {
+            name: "test-workplace".to_owned(),
+        })
+        .to_request();
+    let response = test::call_service(&app, request).await;
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let response_json: Value = test::read_body_json(response).await;
+    let expected_json = json!({
+        "workplace": {
+            "id": 1,
+            "name": "test-workplace",
+        },
+    });
+    assert_eq!(response_json, expected_json);
+
+    let user = User { id: 1.into() };
+    let repository = context.repositories.workplace();
+    let workplaces = repository.list(&user).await.unwrap();
+    assert_eq!(workplaces.iter().count(), 1);
+
+    let workplace = workplaces.first().unwrap();
+    assert_eq!(workplace.name, "test-workplace");
 }
