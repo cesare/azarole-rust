b31b62f4a366da060fc47255449fb7c27f3cb9e7
SAWADA Tadashi <cesare@mayverse.jp>
2026-01-26 10:26:34 +0900

    Add test for creating attendance with other user

--- tests/attendance_records_test.rs
+++ tests/attendance_records_test.rs
@@ -85,3 +85,35 @@ async fn attendance_record_creation(pool: SqlitePool) {
     assert_eq!(attendance_record.event, attendance_record::Event::ClockIn);
     assert_eq!(attendance_record.recorded_at, now.to_utc().into());
 }
+
+#[sqlx::test(fixtures("users", "workplaces"))]
+async fn attendance_record_creation_with_other_user(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let now = Local::now();
+    let cookie_value = common::generate_cookie_value_with_signin_user(2);
+    let request = test::TestRequest::post()
+        .uri("/workplaces/1/attendance_records")
+        .insert_header(("Cookie", cookie_value))
+        .set_form(CreationParams {
+            event: attendance_record::Event::ClockIn,
+            datetime: now,
+        })
+        .to_request();
+    let response = test::call_service(&app, request).await;
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    let attendance_records: Vec<AttendanceRecord> = sqlx::query_as("select id, workplace_id, event, recorded_at from attendance_records where workplace_id = $1 order by created_at desc")
+        .bind(1)
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+    assert_eq!(attendance_records.iter().count(), 0);
+}
