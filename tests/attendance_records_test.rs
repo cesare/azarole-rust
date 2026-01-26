4e8cbac78cb53b7bcb706fb95892dafd7b2fbe73
SAWADA Tadashi <cesare@mayverse.jp>
2026-01-26 10:43:22 +0900

    Add tests for deleting attendances

--- tests/attendance_records_test.rs
+++ tests/attendance_records_test.rs
@@ -117,3 +117,95 @@ async fn attendance_record_creation_with_other_user(pool: SqlitePool) {
         .unwrap();
     assert_eq!(attendance_records.iter().count(), 0);
 }
+
+#[sqlx::test(fixtures("users", "workplaces", "attendance_records"))]
+async fn attendance_record_deletion_without_signin(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let request = test::TestRequest::delete()
+        .uri("/workplaces/1/attendance_records/1")
+        .to_request();
+    let result = test::try_call_service(&app, request).await;
+    assert!(result.is_err());
+
+    let error = result.unwrap_err();
+    let response = error.error_response();
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+
+    let attendance_record: Option<AttendanceRecord> = sqlx::query_as(
+        "select id, workplace_id, event, recorded_at from attendance_records where id = $1",
+    )
+    .bind(1)
+    .fetch_optional(&pool)
+    .await
+    .unwrap();
+
+    assert!(attendance_record.is_some());
+}
+
+#[sqlx::test(fixtures("users", "workplaces", "attendance_records"))]
+async fn attendance_record_deletion(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let cookie_value = common::generate_cookie_value_with_signin_user(1);
+    let request = test::TestRequest::delete()
+        .uri("/workplaces/1/attendance_records/1")
+        .insert_header(("Cookie", cookie_value))
+        .to_request();
+    let response = test::call_service(&app, request).await;
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let attendance_record: Option<AttendanceRecord> = sqlx::query_as(
+        "select id, workplace_id, event, recorded_at from attendance_records where id = $1",
+    )
+    .bind(1)
+    .fetch_optional(&pool)
+    .await
+    .unwrap();
+
+    assert!(attendance_record.is_none());
+}
+
+#[sqlx::test(fixtures("users", "workplaces", "attendance_records"))]
+async fn attendance_record_deletion_with_other_user(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let cookie_value = common::generate_cookie_value_with_signin_user(2);
+    let request = test::TestRequest::delete()
+        .uri("/workplaces/1/attendance_records/1")
+        .insert_header(("Cookie", cookie_value))
+        .to_request();
+    let response = test::call_service(&app, request).await;
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    let attendance_record: Option<AttendanceRecord> = sqlx::query_as(
+        "select id, workplace_id, event, recorded_at from attendance_records where id = $1",
+    )
+    .bind(1)
+    .fetch_optional(&pool)
+    .await
+    .unwrap();
+
+    assert!(attendance_record.is_some());
+}
---
+++ tests/fixtures/attendance_records.sql
@@ -0,0 +1,5 @@
+insert into attendance_records (id, workplace_id, event, recorded_at, created_at)
+values
+  (1, 1, 'clock-in', '2026-01-26T12:34:56Z', '2026-01-26T12:34:56Z'),
+  (2, 1, 'clock-out', '2026-01-26T13:14:15Z', '2026-01-26T13:14:15Z')
+;
