a97629e475bfef35dd3c48635946a9aa03fe8f82
SAWADA Tadashi <cesare@mayverse.jp>
2026-02-02 11:26:55 +0900

    Add tests on creating clock-in/out with an invalid api key

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -55,6 +55,35 @@ async fn clock_in_with_api_key(pool: SqlitePool) {
     assert_eq!(attendance.event, attendance_record::Event::ClockIn);
 }
 
+#[sqlx::test(fixtures("users", "workplaces", "api_keys"))]
+async fn clock_in_with_invalid_api_key(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let invalid_api_key = "invalid-api-key";
+    let request = test::TestRequest::post()
+        .uri("/api/workplaces/1/clock_ins")
+        .insert_header(("Authorization", format!("Bearer {invalid_api_key}")))
+        .to_request();
+    let result = test::try_call_service(&app, request).await;
+    assert!(result.is_err());
+
+    let error = result.unwrap_err();
+    let response = error.error_response();
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+
+    let records: Vec<AttendanceRecord> = sqlx::query_as("select id, workplace_id, event, recorded_at from attendance_records where workplace_id = $1")
+        .fetch_all(&pool)
+        .await.unwrap();
+    assert_eq!(records.iter().count(), 0);
+}
+
 #[sqlx::test(fixtures("users", "workplaces"))]
 async fn clock_out_without_api_key(pool: SqlitePool) {
     let context = common::create_context(pool);
@@ -104,4 +133,33 @@ async fn clock_out_with_api_key(pool: SqlitePool) {
 
     let attendance = records.first().unwrap();
     assert_eq!(attendance.event, attendance_record::Event::ClockOut);
+}
+
+#[sqlx::test(fixtures("users", "workplaces", "api_keys"))]
+async fn clock_out_with_invalid_api_key(pool: SqlitePool) {
+    let context = common::create_context(pool.clone());
+    let app = test::init_service(
+        App::new()
+            .wrap(common::create_session_middleware())
+            .app_data(Data::new(context))
+            .configure(azarole::handlers::routes),
+    )
+    .await;
+
+    let invalid_api_key = "invalid-api-key";
+    let request = test::TestRequest::post()
+        .uri("/api/workplaces/1/clock_outs")
+        .insert_header(("Authorization", format!("Bearer {invalid_api_key}")))
+        .to_request();
+    let result = test::try_call_service(&app, request).await;
+    assert!(result.is_err());
+
+    let error = result.unwrap_err();
+    let response = error.error_response();
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+
+    let records: Vec<AttendanceRecord> = sqlx::query_as("select id, workplace_id, event, recorded_at from attendance_records where workplace_id = $1")
+        .fetch_all(&pool)
+        .await.unwrap();
+    assert_eq!(records.iter().count(), 0);
 }
