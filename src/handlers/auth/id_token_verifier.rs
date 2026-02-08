bf210f13089e14d730c8c79d41e17e5051141b73
SAWADA Tadashi <cesare@mayverse.jp>
2026-02-08 09:49:59 +0900

    Refactor internal methods

--- src/handlers/auth/id_token_verifier.rs
+++ src/handlers/auth/id_token_verifier.rs
@@ -35,10 +35,10 @@ impl<'a> IdTokenVerifier<'a> {
     }
 
     pub(super) async fn verify(self) -> Result<Claims, AuthError> {
-        let key_id = self.find_key_id()?;
+        let key_id = self.find_key_id(self.token)?;
         let jwks = self.jwks.fetch().await?;
         match jwks.find(&key_id) {
-            Some(jwk) => self.verify_id_token(jwk),
+            Some(jwk) => self.verify_id_token(jwk, self.token, self.nonce),
             None => {
                 log::error!("Failed to find key_id {} in jwk", key_id);
                 Err(AuthError::InvalidIdToken)
@@ -46,8 +46,8 @@ impl<'a> IdTokenVerifier<'a> {
         }
     }
 
-    fn find_key_id(&self) -> Result<String, AuthError> {
-        let header = decode_header(self.token)
+    fn find_key_id(&self, token: &str) -> Result<String, AuthError> {
+        let header = decode_header(token)
             .inspect_err(|e| log::error!("Failed to decode token header: {:?}", e))?;
 
         match header.kid {
@@ -59,7 +59,7 @@ impl<'a> IdTokenVerifier<'a> {
         }
     }
 
-    fn verify_id_token(&self, jwk: &Jwk) -> Result<Claims, AuthError> {
+    fn verify_id_token(&self, jwk: &Jwk, token: &str, nonce: &str) -> Result<Claims, AuthError> {
         let decoding_key = DecodingKey::from_jwk(jwk)
             .inspect_err(|e| log::error!("Failed to detect decoding key from jwk: {:?}", e))?;
 
@@ -68,12 +68,12 @@ impl<'a> IdTokenVerifier<'a> {
         let mut validation = Validation::new(Algorithm::RS256);
         validation.set_audience(&[client_id]);
 
-        let jwt = decode::<Claims>(&self.token, &decoding_key, &validation)
+        let jwt = decode::<Claims>(token, &decoding_key, &validation)
             .inspect_err(|e| log::error!("Failed to decode claims from token: {:?}", e))?;
 
         let claims = jwt.claims;
 
-        if claims.nonce != self.nonce {
+        if claims.nonce != nonce {
             return Err(AuthError::InvalidIdToken);
         }
 
